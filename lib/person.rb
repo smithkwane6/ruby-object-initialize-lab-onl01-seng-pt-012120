6ece4978567ac62e5c9172252f2eae55221b5d7e
Kwane Smith <smithkwane6@gmail.com>
2020-02-07 15:17:38 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+# the class is Person
+# object initialization
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
