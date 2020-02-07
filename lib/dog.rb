6c6a39aaa5b595f4af5deffa3d2c6c7d44a50a2e
Kwane Smith <smithkwane6@gmail.com>
2020-02-07 15:01:31 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Person
+# the class is Person
+# object initialization
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
+
